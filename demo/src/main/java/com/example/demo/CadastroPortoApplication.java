f6384fa9efcb7dc40bd93adec7e72dc18adcb20b
caioabmoura <caio.abezerram@gmail.com>
2025-03-30 18:41:02 -0300

    first commit

---
+++ demo/src/main/java/com/example/demo/CadastroPortoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroPortoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroPortoApplication.class, args);
+	}
+
+}
